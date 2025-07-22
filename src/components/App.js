2aadb96521663f98f02bbadf18a408dfb553046d
chethan096 <chethankl414@gmail.com>
2025-07-22 12:31:13 +0530

    Connected app to live JSON server on Render

--- src/components/App.js
+++ src/components/App.js
@@ -89,7 +89,7 @@ export default function App() {
   );
 
   useEffect(() => {
-    fetch("http://localhost:8000/questions")
+    fetch("https://the-react-quiz-2.onrender.com/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
